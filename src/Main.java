f57259630edef059984e43b96e375ff461bb9c65
akashi-gupta <akashi.gupta_cs18@gla.ac.in>
2021-04-14 13:47:31 +0530

    print once more

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         Constructor c = new Constructor(10);
+        c.printData();
         c.printData();
     }
 }
